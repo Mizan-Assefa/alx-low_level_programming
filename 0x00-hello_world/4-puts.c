9a1d9153fd120f8e663deef23d54009854d4264f
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-07 04:06:38 -0700

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/* include to gate all element
+ * stdio.h for input out put strim
+ * main for all funnctionwe ned
+ * print the content 
+ * returnt o to end 
+ */
+ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
