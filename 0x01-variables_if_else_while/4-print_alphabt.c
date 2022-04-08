55e0cbd17c9dbeb7d48fb0a97b46732237562381
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 12:56:55 -0700

    task04

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/**
+ * main -main block
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	char lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+	{
+	if (lower_case == 'q')
+		continue;
+	else if (lower_case == 'e')
+		continue;
+	}
+	putchar(lower_case);
+
+	putchar('\n');
+
+	return (0);
+
+}
