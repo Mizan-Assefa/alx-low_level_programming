12de1e94fe4b54de90cc5bdc0f5704a461e72ec4
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 12:31:33 -0700

    task03

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,16 +3,16 @@
  * main -main block
  * Return: Always 0 sucess
  */
-int main (void)
+int main(void)
+
 {
 	char lower_case;
 	char uper_case;
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
-		putchar(lower_case);
+		putchar(lower_case');
 	}
-
 	for (uper_case = 'A'; uper_case <= 'Z'; uper_case++)
 	{
 		putchar(uper_case);
