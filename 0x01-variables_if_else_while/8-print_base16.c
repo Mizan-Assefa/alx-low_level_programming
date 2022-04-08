9e4051617bf3cc96dd6713036d67e3aec8f7392a
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 14:37:01 -0700

    task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,15 @@ int main(void)
 {
 	char hexa;
 
-	for (hexa = 40; hexa <=57; hexa++)
+	for (hexa = 40; hexa <= 57; hexa++)
 	{
 		putchar(hexa);
 	}
+	for (hexa = 97; hexa <= 102; hexa++)
+	{
+		putchar(hexa);
+	}
+
 	putchar('\n');
 
 	return (0);
