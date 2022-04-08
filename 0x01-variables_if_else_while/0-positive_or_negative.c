d917a8b03ad91ad2451c954569d8e3646878891b
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 03:33:39 -0700

    task01

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -18,13 +18,15 @@ int main(void)
 	{
 		printf("%i is positive\n", n);
 	}
+
 	else if (n == 0)
 	{
 		printf("%i is zero\n", n);
 	}
-	else 
+
+	else	
 	{
-		printf("%i is negative\n", n);
+		printf("%i is negative\n",n);
 	}
 
 	return (0);
