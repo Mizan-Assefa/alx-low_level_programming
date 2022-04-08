7b449d7b8d9c1c4785632846244c8453cefb8786
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 13:25:48 -0700

    task05

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *  main - main block
+ *
+ *  Return: Always 0 secess
+ */
+int main(void)
+
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%digit", number);
+	}
+	printf("\n");
+
+	 return (0);
+
+}
