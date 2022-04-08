c8a390f5e969e95f931f7730471e51b286deded7
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 14:04:37 -0700

    task7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *  main -main block
+ *
+ *  Return: Always 0 sucess
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a <= 'a'; a++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
