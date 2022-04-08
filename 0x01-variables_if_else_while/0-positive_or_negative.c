d100effcc222c690e4d5f3921fe054cc5d9086ea
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 00:46:25 -0700

    task0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<time.h>
+
+/**
+ * main- mainblock
+ *
+ * Return- return value
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if(n>0)
+	{
+		printf("is positive\n");
+	}
+	else if(n==0)
+	{
+		printf("is zero\n");
+	}
+	else 
+	{
+		printf("is negative\n");
+	}
+	return (0);
+}
+
