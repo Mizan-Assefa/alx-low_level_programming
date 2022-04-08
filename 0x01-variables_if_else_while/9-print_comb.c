b27470eecb6aeee0a64c79e42e4f66af4aaa4f60
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-08 14:49:50 -0700

    task9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  main -main block
+ *
+ *  Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; c++)
+	{
+		putchar(c);
+		if (a != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+
+}
