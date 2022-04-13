45a8f88bf1d26bf0517ce0b9f485f2cfe02f36b0
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-13 00:08:50 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,7 +31,19 @@ void times_table(void)
 					_putchar (',');
 					_putchar (' ');
 				}
+
 			}
+			else
+			{
+				_putchar ((c / 10) + '0');
+				_putchar ((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar (',');
+					_putchar (' ');
+				}
+			}
+		
 
 		}
 		_putchar ('\n');
