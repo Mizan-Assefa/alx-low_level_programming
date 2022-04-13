140859ba63d20f53e27f4c3a6529705dc0d6df8e
Mizan-Assefa <mizanassefa29@gmail.com>
2022-04-13 02:22:41 -0700

    task00

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *  main -tast function that prints if integer is postive or negative
+ *  Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	postive_or_negative(i);
+
+	return (0);
+
+}
---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+my readme for c programing tasx 03
