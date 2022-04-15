2dd43b470a4aef3e7ca1b2c055a68a58310a53cc
Mizan-Assefa <99024318+Mizan-Assefa@users.noreply.github.com>
2022-04-15 11:59:13 +0300

    Update 101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,3 +6,19 @@
 */
 void print_number(int n)
 {
+  unsigned int l;
+  
+  if (n < 0)
+  {
+    _putchar (' '); 
+    l = -n;
+  }
+  else
+  {
+    l = n;
+  }
+  if (l / 10 !=0)
+  print_number (l / 10);
+  _putchar (l % 10) + '0');
+}
+              
