7b800f1cdc8225331a92fc10fa3322b37a5f1c88
Mizan-Assefa <99024318+Mizan-Assefa@users.noreply.github.com>
2022-05-19 21:17:35 +0300

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,21 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 /**
- * print_list - prints all the elements  a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
-size_t print_list(const list_t *h)
-{
-size_t num;
-num = 0;
-while (h != NULL)
-{
-if (h->str == NULL)
-printf("[%u] %s\n", 0, "(nil)");
-else
-printf("[%u] %s\n", h->len, h->str);
-h = h->next;
-num++;
-}
-return (num);
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */ 
+int main(void) {
+    list_t *head;
+    list_t *new;
+    list_t hello = {"World", 5, NULL};
+    size_t n;
+    head = &hello;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+    {
+        printf("Error\n");
+        return (1);
+    }
+    new->str = strdup("Hello");
+    new->len = 5;
+    new->next = head;
+    head = new;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+    printf("\n");
+    free(new->str);
+    new->str = NULL;
+    n = print_list(head);
+    printf("-> %lu elements\n", n);
+    free(new);
+    return (0);
 }
