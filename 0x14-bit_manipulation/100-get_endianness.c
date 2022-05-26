f6cf5bef0f0b981cd1bd6e2080fba12ae952bc52
Mizan-Aassefa <mizanassefa29@gmail.com>
2022-05-26 10:48:02 -0700

    task06

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,20 +1,12 @@
 #include "main.h"
 /**
- *  binary_to_uint - convert binary to unsigned int
- *  @b: binary number
- *
+ *  get_endianness - check
  *  Return: the number to be returneed
  */
-unsigned int flip_bits(unsigned long int n, unsigned long int m)
+int get_endianness(void)
 {
-	int i, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
-	for (i = 0; i >= 0; i--)
-		current = exclusive >> i;
-	if (current & 1)
-		count++;
-}
+	unsigned int i = 1;
+	char *c = (char *)&i;
 
-return (count);
+	return (*c);
 }
